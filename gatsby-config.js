6a3eb8b57a48c31b4e479ea1a4512e944d8ae527
Peter Lin <peterlin.california@gmail.com>
2020-02-24 20:18:23 -0800

    starting to port html to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+ /*
+  * Configure your Gatsby site with this file.
+  *
+  * See: https://www.gatsbyjs.org/docs/gatsby-config/
+  */
 
-module.exports = {
-  /* Your site config here */
-}
+ module.exports = {
+   plugins: ["gatsby-plugin-react-helmet"],
+ }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,81 @@
 import React from "react"
+import "../styles/custom.css"
+import { Link } from "gatsby"
+import Helmet from "react-helmet"
+export default () => ( <>
+    <Helmet>
+      <title>Taylor's Tidy Trees</title>
+      <link to="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    </Helmet>
 
-export default () => <div>Hello world!</div>
+
+
+    <header>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-custom" style="border-bottom:1px solid black;">
+      <div className="container">
+
+        <Link className="navbar-brand" to="./home.html">
+          <img src ="../photos/lin.png" className="logo" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="../home.html">Home
+                <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="about.html">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="gallery.html">Gallery</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="bananas.html">Bananas?</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="contactme.html">Contact Me</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav></header>
+    <main>
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-custom" style="border-bottom:1px solid black;">
+      <div className="container">
+
+        <a className="navbar-brand" to="./home.html">
+          <img src ="../photos/lin.png" className="logo" />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" to="../home.html">Home
+                <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" to="about.html">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" to="gallery.html">Gallery</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" to="bananas.html">Bananas?</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" to="contactme.html">Contact Me</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </main>
+  </>
+)
